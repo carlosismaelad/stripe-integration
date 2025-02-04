0a292e19e8dc318d3b8b0ece62ab647e03b39661
Carlos Dourado <carlosismaelad@outlook.com>
2025-02-03 23:28:56 -0300

    create project

---
+++ stripe-integration/src/test/java/com/api/stripe/integration/stripe_integration/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.stripe.integration.stripe_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
